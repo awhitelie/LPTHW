bf5d1f80093a7e02ec70e366c61fe959089ec618
awhitelie <czegel@gmail.com>
2015-07-26 10:47:01 -0400

    \\

--- ex17.py
+++ ex17.py
@@ -3,11 +3,9 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "COpy from %s to %s" % (from_file, to_file)
+print "Copy from %s to %s" % (from_file, to_file)
 
-# we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
 print "The input file is %d bytes long." % len(indata)
 
@@ -21,4 +19,3 @@ out_file.write(indata)
 print "Alright, all done."
 
 out_file.close()
-in_file.close()
